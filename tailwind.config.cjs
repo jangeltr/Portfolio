bea10a69e6ce460eda522e3c7dcf823f404c6be1
Angel Torres <jangeltr@hotmail.com>
2023-09-05 11:48:29 -0600

    cambios parciales del 5 Sep 23

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			spacing: {
+				'128':'36rem', //para el tamaño de la imagen
+			}
+		},
 	},
 	plugins: [
 		require('@tailwindcss/forms'),
