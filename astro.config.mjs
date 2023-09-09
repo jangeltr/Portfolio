3fa3cb196532a11650858ca200491d36c38cb70e
Angel Torres <jangeltr@hotmail.com>
2023-09-09 16:14:59 -0600

    cambios parciales del 9 Sep 23

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 import { defineConfig } from 'astro/config';
+import nodejs from '@astrojs/node'; // for server-side rendering
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: 'http://localhost/jangeltr/',
-  buildOptions: {
-    baseUrl: '/MiApp/'
-  },
-  serveOptions: {
-    // port: 3000,
-    baseUrl: '/MiApp/'
-  }
+  // for server-side rendering
+  // output: 'server',
+  // adapter: nodejs({
+  //   mode: 'standalone'
+  // }),
+  
+  // Esto sigue sin funcionarme
+  // buildOptions: {
+  //   baseUrl: '/MiApp/'
+  // },
+  // serveOptions: {
+  //   port: 3000,
+  //   baseUrl: '/MiApp/'
+  // },
 });
